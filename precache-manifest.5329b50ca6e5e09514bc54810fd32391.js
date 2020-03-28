da57f7b298f21964aa8d63651d179e7f744919c8
Jeffrey Meesters <jmeesters@harlemnext.com>
2020-03-28 15:46:41 +0100

    Updates

---
+++ precache-manifest.5329b50ca6e5e09514bc54810fd32391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e102a5537dc469b0a2ee057b0cd7288e",
+    "url": "/Udemy-react2020-monster-roladex/index.html"
+  },
+  {
+    "revision": "79b7000a1675d89c7809",
+    "url": "/Udemy-react2020-monster-roladex/static/css/main.8c2e8256.chunk.css"
+  },
+  {
+    "revision": "9d36cb1448d0cd9bbf34",
+    "url": "/Udemy-react2020-monster-roladex/static/js/2.9d52131b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Udemy-react2020-monster-roladex/static/js/2.9d52131b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b7000a1675d89c7809",
+    "url": "/Udemy-react2020-monster-roladex/static/js/main.9447bcd7.chunk.js"
+  },
+  {
+    "revision": "03131e7db379f3426571",
+    "url": "/Udemy-react2020-monster-roladex/static/js/runtime-main.29435a1a.js"
+  }
+]);
