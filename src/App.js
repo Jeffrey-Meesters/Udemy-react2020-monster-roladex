8c071485c059e3a50597ae3de1e69f90824cb183
Jeffrey Meesters <jmeesters@harlemnext.com>
2020-03-28 16:29:49 +0100

    App doesnt need to know about monsters (in this setup) so I moved it down and made App a functional component

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
-import React, { Component } from "react";
-import { CardList } from "./components/card/card-list/card-list.component";
-import { SearchBox } from "./components/searchbox/search-box.component";
+import React from "react";
+import CardList from "./components/card/card-list/card-list.component";
+
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      monsters: [],
-      search: ""
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(resp => resp.json())
-      .then(monsters =>
-        this.setState({
-          monsters: monsters
-        })
-      );
-  }
-
-  // es6 binds this so we don't need to bind in the constructor
-  handleChange = e => {
-    this.setState({ search: e.target.value });
-  };
-
-  render() {
-    const { monsters, search } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(search.toLocaleLowerCase())
-    );
-
-    return (
-      <div className="App">
-        <h1 className="app-title">Monster Roladex</h1>
-        <SearchBox
-          placeholder="Search monsters"
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="app-title"> Monster Roladex </h1>
+      <CardList />
+    </div>
+  );
+};
 
 export default App;
--- src/components/card/card-list/card-list.component.jsx
+++ src/components/card/card-list/card-list.component.jsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card } from "./../card/card.component";
+import { SearchBox } from "./../../searchbox/search-box.component";
 import "./card-list.styles.css";
 
-export const CardList = props => {
-  return (
-    <div className="card-list">
-      {props.monsters.map(monster => (
-        <Card key={monster.id} monster={monster} />
-      ))}
-    </div>
-  );
-};
+class CardList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      search: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(resp => resp.json())
+      .then(monsters =>
+        this.setState({
+          monsters: monsters
+        })
+      );
+  }
+
+  // es6 binds this so we don't need to bind in the constructor
+  handleChange = e => {
+    this.setState({
+      search: e.target.value
+    });
+  };
+
+  render() {
+    const { monsters, search } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+
+    return (
+      <>
+        <SearchBox
+          placeholder="Search monsters"
+          handleChange={this.handleChange}
+        />
+        <div className="card-list">
+          {filteredMonsters.map(monster => (
+            <Card key={monster.id} monster={monster} />
+          ))}
+        </div>
+      </>
+    );
+  }
+}
+
+export default CardList;
