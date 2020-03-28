f917213312986c0b03342b4f8cc897c9f1feead2
Jeffrey Meesters <jmeesters@harlemnext.com>
2020-03-28 11:20:55 +0100

    Building components and props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { CardList } from "./components/card/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -18,9 +19,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.username}>{monster.username}</h1>
-        ))}
+        <CardList monsters="{this.monsters}">
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.username}>{monster.username}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
